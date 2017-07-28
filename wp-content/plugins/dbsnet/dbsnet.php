4b5e0f06a77360047de227b3682ed7530e4fe819
insomynwa <pikamons@gmail.com>
2017-07-29 02:38:51 +0700

    clean comment

--- wp-content/plugins/dbsnet/dbsnet.php
+++ wp-content/plugins/dbsnet/dbsnet.php
@@ -5,44 +5,6 @@ Description: Plugin for Dibuang Sayang
 Version: 1.0
 */
 
-// class DBSnetPluginSkeleton {
-
-// 	public function __construct(){
-// 		// Add Js & CSS for admin screen
-// 		//add_action('admin_enqueue_scripts', array($this, 'enqueueAdmin'));
-
-// 		// Add Js & CSS for front-end display
-// 		//add_action('wp_enqueue_scripts', array($this, 'enqueue'));
-
-// 		// Sample menu
-// 		add_action( 'admin_menu', array($this, 'renderMenu') );
-// 	}
-
-// 	public function renderMenu(){
-// 		add_menu_page( 'DBS Plugin Page', 'DBS Plugin Menu', 'manage_options', 'dbs-plugin', '', '', 3 );
-
-// 		add_submenu_page(
-// 			'dbs-plugin',
-// 			'Pemesanan',
-// 			'Pemesanan',
-// 			'manage_options',
-// 			'dbs-pemesanan',
-// 			 array( $this, 'RenderPage')
-// 			);
-// 	}
-
-// 	function RenderPage(){
-// 		echo "<div>test page menu</div>";
-// 	}
-
-// }
-
-
-// $skeleton = new DBSnetPluginSkeleton();
-
-//=========================================================================================
-//=========================================================================================
-
 if (! defined( 'WPINC' )) {
 	die;
 }
