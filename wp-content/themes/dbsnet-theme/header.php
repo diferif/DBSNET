daaa97e84305a6eed58dfdd5c8ff876d80c9573e
insomynwa <pikamons@gmail.com>
2017-07-30 00:27:40 +0700

    top navbar design v 0.1.0

--- wp-content/themes/dbsnet-theme/header.php
+++ wp-content/themes/dbsnet-theme/header.php
@@ -30,18 +30,28 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <?php if(!is_user_logged_in()): ?>
+            <li><a href="#">Daftar</a></li>
+            <li><a href="#">Masuk</a></li>
+            <?php else: ?>
+            <?php $user_info = get_userdata(get_current_user_id()); ?>
+            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Keranjang <span class="badge">5</span></a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-th" aria-hidden="true"></span> <?php _e($user_info->first_name); ?> <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Kotak Surat <span class="badge">5</span></a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Profil</a></li>
+                <li><a href="<?php current_user_can('manage_options') ? _e(admin_url()) : _e('#'); ?>"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Dashboard</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="<?php echo wp_logout_url(home_url()); ?>"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Keluar</a></li>
+              </ul>
+            </li>
+            <?php endif; ?>
+          </ul>
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
