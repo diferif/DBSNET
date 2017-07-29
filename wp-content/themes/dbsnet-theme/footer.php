af42b42690ce127d01a09d10df9eccf4bc9e69e0
insomynwa <pikamons@gmail.com>
2017-07-29 19:29:00 +0700

    structuring

---
+++ wp-content/themes/dbsnet-theme/footer.php
@@ -0,0 +1,13 @@
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- wp-content/themes/dbsnet-theme/functions.php
+++ wp-content/themes/dbsnet-theme/functions.php
@@ -10,8 +10,14 @@ function dbsnettheme_enqueue_scripts(){
 	$dependencies = array( 'jquery' );
 	wp_enqueue_script('boot', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.7', false );
 }
+
 add_action('wp_enqueue_scripts', 'dbsnettheme_enqueue_styles');
 add_action('wp_enqueue_scripts', 'dbsnettheme_enqueue_scripts');
+
+function dbsnettheme_wp_setup() {
+	add_theme_support('title-tag');
+}
+add_action('after_setup_theme','dbsnettheme_wp_setup');
 
 
 ?>
