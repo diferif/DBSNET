74c860c81b6c16f4db594051b96a40bad5609386
insomynwa <pikamons@gmail.com>
2017-07-30 16:28:05 +0700

    tenant slideshow v 0.1.0

--- wp-content/themes/dbsnet-theme/header.php
+++ wp-content/themes/dbsnet-theme/header.php
@@ -80,5 +80,48 @@
     </div>
 
     <div class="container">
+      <div class="slideshow-container">
+        <div class="mySlides fade">
+          <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width: 100%">
+          <!-- <div class="slideshow-text">Tenant 1</div> -->
+        </div>
+        <div class="mySlides fade">
+          <img src="https://www.w3schools.com/howto/img_fjords_wide.jpg" style="width: 100%">
+          <!-- <div class="slideshow-text">Tenant 2</div> -->
+        </div>
+        <div class="mySlides fade">
+          <img src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width: 100%">
+          <!-- <div class="slideshow-text">Tenant 3</div> -->
+        </div>
+      </div>
+      <br>
+      <!-- <div style="text-align: center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div> -->
+    </div>
+    <script>
+      var slideIndex = 0;
+      showSlides();
+      function showSlides(){
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        // var dots = document.getElementsByClassName("dot");
+        for(i = 0; i < slides.length; i++){
+          slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if(slideIndex > slides.length){slideIndex=1}
+        // for(i = 0; i < dots.length; i++){
+        //   dots[i].className = dots[i].className.replace(" slideshow-active","");
+        // }
+        slides[slideIndex-1].style.display = "block";
+        // dots[slideIndex-1].className += " slideshow-active";
+        setTimeout(showSlides,3000);
+      }
+    </script>
+
+    <div class="container">
       <!-- Example row of columns -->
       <div class="row">       
